34f284b017efa75811bac92682362d9547602d42
kodai-021 <masamichinoago@gmail.com>
2025-06-08 17:49:04 +0900

    CDKハンズオン_#4

--- lib/first_cdk_work-stack.ts
+++ lib/first_cdk_work-stack.ts
@@ -7,9 +7,12 @@ export class FirstCdkWorkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const systemName = this.node.tryGetContext('systemName');
+    const envType = this.node.tryGetContext('envType');
+
      new CfnVPC(this,'Vpc' ,{
         cidrBlock: '10.0.0.0/16',
-        tags: [{key: 'Name',value:'devio-stg-vpc'}]
+        tags: [{key: 'Name',value:`${systemName}-${envType}-vpc`}]
      });
 
   }
