bd1cb58bff3c2c52fbe3029d7aaf3030c4585666
kodai-021 <masamichinoago@gmail.com>
2025-06-08 17:00:46 +0900

    CDKハンズオン_#2

--- lib/first_cdk_work-stack.ts
+++ lib/first_cdk_work-stack.ts
@@ -1,20 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Vpc } from 'aws-cdk-lib/aws-ec2'; // <- 追加
+import { CfnVPC } from 'aws-cdk-lib/aws-ec2'; // <- 追加
 
 export class FirstCdkWorkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-     new Vpc(this, 'Vpc'); // <- 追加
+     new CfnVPC(this,'Vpc' ,{
+        cidrBlock: '10.0.0.0/16',
+        tags: [{key: 'Name',value:'devio-stg-vpc'}]
+     });
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'FirstCdkWorkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
 
