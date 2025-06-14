de77c0df834bf42392634743f84c3f7402cf5587
kodai-021 <masamichinoago@gmail.com>
2025-06-14 17:21:23 +0900

    CDKハンズオン_#8

--- lib/resource/vpc.ts
+++ lib/resource/vpc.ts
@@ -1,18 +1,20 @@
 import { CfnVPC } from 'aws-cdk-lib/aws-ec2';
-import {Construct} from 'constructs';
+import { Construct } from 'constructs';
+import { Resource } from './abstract/resource';
 
-export class Vpc {
+
+export class Vpc extends Resource {
     public vpc: CfnVPC;
 
-    constructor() { };
+    constructor() {
+        super();
+     };
 
-    public createResources(scope: Construct) {
-        const systemName = scope.node.tryGetContext('systemName');
-        const envType = scope.node.tryGetContext('envType');
+    createResources(scope: Construct) {
 
         this.vpc = new CfnVPC(scope, 'Vpc', {
             cidrBlock: '10.0.0.0/16',
-            tags: [{ key: 'Name', value: `${systemName}-${envType}-vpc` }]
+            tags: [{ key: 'Name', value: this.createResourceName(scope,'vpc') }]
         });
     }
 }
